345e58621f38fda25917206150d1f64cd123aded
llfiji <shift0x00@gmail.com>
2025-07-22 11:24:43 -0400

    create demo mcp server

---
+++ api/tools/get-expert-knowledge.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+export default {
+    name: "search-expert-knowledge",
+    description: "Searches for expert knowledge related to the given topic / question. Expert knowledge is used to enrich reasoning or answer with authoritative, domain-specific insights that are not found in search engines",
+    schema: {
+        query: z.string()
+    },
+    callback: async (query: string) : Promise<any> => {
+       return {
+            content: [
+                { 
+                    type: 'text', 
+                    text:  query 
+                }
+            ],
+        }
+    }
+}
